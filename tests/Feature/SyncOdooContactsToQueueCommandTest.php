151e06a29d79d9c1523789296efd4be0d8ad646a
matsuyuna <matsuyuna@users.noreply.github.com>
2026-03-17 22:46:09 -0400

    Enviar productos de la última orden en ultimo_producto_comprado

--- tests/Feature/SyncOdooContactsToQueueCommandTest.php
+++ tests/Feature/SyncOdooContactsToQueueCommandTest.php
@@ -154,6 +154,54 @@ class SyncOdooContactsToQueueCommandTest extends TestCase
         ]);
     }
 
+    public function test_ultimo_producto_comprado_incluye_todos_los_productos_de_la_ultima_orden_separados_por_coma(): void
+    {
+        putenv('ODOO_URL=https://odoo.test');
+        putenv('ODOO_DB=test_db');
+        putenv('ODOO_USERNAME=test_user');
+        putenv('ODOO_PASSWORD=test_pass');
+
+        Http::fake([
+            'https://odoo.test/xmlrpc/2/common' => Http::response($this->authXml(7), 200),
+            'https://odoo.test/xmlrpc/2/object' => function ($request) {
+                $body = $request->body();
+
+                if (str_contains($body, '<string>res.partner</string>') && str_contains($body, '<string>search_read</string>')) {
+                    if (str_contains($body, '<name>offset</name><value><int>0</int></value>')) {
+                        return Http::response($this->partnersXml(), 200);
+                    }
+
+                    if (str_contains($body, '<name>commercial_partner_id</name>')) {
+                        return Http::response($this->commercialPartnerXml(), 200);
+                    }
+
+                    return Http::response($this->emptyArrayXml(), 200);
+                }
+
+                if (str_contains($body, '<string>sale.order</string>') && str_contains($body, '<string>search_read</string>')) {
+                    return Http::response($this->multiProductLatestSaleOrdersXml(), 200);
+                }
+
+                if (str_contains($body, '<string>sale.order.line</string>') && str_contains($body, '<string>search_read</string>')) {
+                    return Http::response($this->multiProductLatestSaleOrderLinesXml(), 200);
+                }
+
+                if (str_contains($body, '<string>pos.order</string>') && str_contains($body, '<string>search_read</string>')) {
+                    return Http::response($this->emptyArrayXml(), 200);
+                }
+
+                return Http::response($this->emptyArrayXml(), 200);
+            },
+        ]);
+
+        $this->artisan('odoo:contacts:pull --batch-size=1 --max-total=1')->assertSuccessful();
+
+        $this->assertDatabaseHas('odoo_contact_syncs', [
+            'odoo_contact_id' => 101,
+            'ultimo_producto_comprado' => 'Vitamina C 1g, Zinc 50mg, Magnesio 400mg',
+        ]);
+    }
+
     private function authXml(int $uid): string
     {
         return <<<XML
@@ -280,6 +328,90 @@ XML;
                 <member><name>order_id</name><value><array><data><value><int>5001</int></value><value><string>S0001</string></value></data></array></value></member>
                 <member><name>product_id</name><value><array><data><value><int>77</int></value><value><string>Acetaminofén 500mg</string></value></data></array></value></member>
                 <member><name>product_uom_qty</name><value><double>2.0</double></value></member>
+              </struct>
+            </value>
+          </data>
+        </array>
+      </value>
+    </param>
+  </params>
+</methodResponse>
+XML;
+    }
+
+    private function multiProductLatestSaleOrdersXml(): string
+    {
+        return <<<'XML'
+<?xml version="1.0"?>
+<methodResponse>
+  <params>
+    <param>
+      <value>
+        <array>
+          <data>
+            <value>
+              <struct>
+                <member><name>id</name><value><int>7001</int></value></member>
+                <member><name>partner_id</name><value><array><data><value><int>900</int></value><value><string>Casa Matriz</string></value></data></array></value></member>
+                <member><name>date_order</name><value><string>2026-03-13 10:30:00</string></value></member>
+              </struct>
+            </value>
+            <value>
+              <struct>
+                <member><name>id</name><value><int>7000</int></value></member>
+                <member><name>partner_id</name><value><array><data><value><int>900</int></value><value><string>Casa Matriz</string></value></data></array></value></member>
+                <member><name>date_order</name><value><string>2026-03-10 08:00:00</string></value></member>
+              </struct>
+            </value>
+          </data>
+        </array>
+      </value>
+    </param>
+  </params>
+</methodResponse>
+XML;
+    }
+
+    private function multiProductLatestSaleOrderLinesXml(): string
+    {
+        return <<<'XML'
+<?xml version="1.0"?>
+<methodResponse>
+  <params>
+    <param>
+      <value>
+        <array>
+          <data>
+            <value>
+              <struct>
+                <member><name>id</name><value><int>9101</int></value></member>
+                <member><name>order_id</name><value><array><data><value><int>7001</int></value><value><string>SO7001</string></value></data></array></value></member>
+                <member><name>product_id</name><value><array><data><value><int>501</int></value><value><string>Vitamina C 1g</string></value></data></array></value></member>
+                <member><name>product_uom_qty</name><value><double>1</double></value></member>
+              </struct>
+            </value>
+            <value>
+              <struct>
+                <member><name>id</name><value><int>9102</int></value></member>
+                <member><name>order_id</name><value><array><data><value><int>7001</int></value><value><string>SO7001</string></value></data></array></value></member>
+                <member><name>product_id</name><value><array><data><value><int>502</int></value><value><string>Zinc 50mg</string></value></data></array></value></member>
+                <member><name>product_uom_qty</name><value><double>1</double></value></member>
+              </struct>
+            </value>
+            <value>
+              <struct>
+                <member><name>id</name><value><int>9103</int></value></member>
+                <member><name>order_id</name><value><array><data><value><int>7001</int></value><value><string>SO7001</string></value></data></array></value></member>
+                <member><name>product_id</name><value><array><data><value><int>503</int></value><value><string>Magnesio 400mg</string></value></data></array></value></member>
+                <member><name>product_uom_qty</name><value><double>2</double></value></member>
+              </struct>
+            </value>
+            <value>
+              <struct>
+                <member><name>id</name><value><int>9104</int></value></member>
+                <member><name>order_id</name><value><array><data><value><int>7000</int></value><value><string>SO7000</string></value></data></array></value></member>
+                <member><name>product_id</name><value><array><data><value><int>504</int></value><value><string>Producto viejo</string></value></data></array></value></member>
+                <member><name>product_uom_qty</name><value><double>1</double></value></member>
               </struct>
             </value>
           </data>
